5ec2d2e099742094ddfc417b90e70af64ff5ef06
rossiaugusto <rossiaugusto@hotmail.com>
2021-09-18 14:49:38 -0300

    commit

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
 ### O 1° é o jogo do dinossauro quando se está sem internet;
 
-### O 2° é um jogo Space Shooter;
+### O 2° é um jogo Genius;
 
-### O 3° é um jogo Genius;
+### O 3° é um jogo da Memória;
 
-### O 4° é um jogo da Memória;
+### O 4°  é a minha versão, modificada, da página inicial do Netflix, a qual eu dei o nome de Basketflix;
 
-### O 5°  é a minha versão, modificada, da página inicial do Netflix, a qual eu dei o nome de Basketflix;
+### O 5° é a recriação da página de login do Instagram;
 
-### O 6° é a recriação da página de login do Instagram;
+### O 6° é um jogo de resgate;
 
-### O 7° é um jogo de resgate;
+### O 7° é o jogo da cobrinha com Java Script;
 
-### O 8° é o jogo da cobrinha com JavaScript e
+### O 8° é um jogo Space Shooter e
 
 ### O 9° é um jogo da velha.
---
+++ Space Shooter/js/script.js
@@ -0,0 +1,76 @@
+const yourShip = document.querySelector('.hero');
+const playArea = document.querySelector('#mainArea');
+var somLaser=document.getElementById("somLaser");
+
+// Movimento e Tiro da Nave
+function flyShip(event) {
+    if(event.key === 'ArrowUp'){
+        event.preventDefault();
+        moveUp();
+    }else if(event.key === 'ArrowDown'){
+        event.preventDefault();
+        moveDown();
+    }else if (event.key === " ") {
+        event.preventDefault();
+        fireLaser();
+    }
+}
+
+//função de subir
+function moveUp() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "0px") {
+        return
+    } else {
+        let position = parseInt(topPosition);
+        position -= 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+//função de descer
+function moveDown() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "550px"){
+        return
+    } else {
+        let position = parseInt(topPosition);
+        position += 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+//Funcionalidade de Tiro
+function fireLaser() {
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser(laser);
+    somLaser.play();
+}
+
+function createLaserElement() {
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser =  document.createElement('img');
+    newLaser.src = 'img/shoot.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left = `${xPosition}px`;
+    newLaser.style.top = `${yPosition - 10}px`;
+    return newLaser;
+}
+
+function moveLaser(laser) {
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left);
+        
+        if (xPosition === 340) {
+            laser.remove();
+        }else{
+            laser.style.left = `${xPosition + 8}px` 
+        }
+    }, 10);
+}
+
+
+
+window.addEventListener('keydown', flyShip);
